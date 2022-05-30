8cc86a853861a39138597da24e0d13e6d4d2e2c3
noritaka <noritaka.kashima@gmail.com>
2022-05-30 11:30:36 +0900

    fs used

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ import fs
 
 # 認証ユーザーを作る
 authorizer = pyftpdlib.authorizers.DummyAuthorizer()
-authorizer.add_user('user', 'password', r"C:\Users\noritaka\ftp", perm='elradfmw')
-authorizer.add_anonymous(r"C:\Users\noritaka\ftp", perm='elradfmw')
+authorizer.add_user('user', 'password', r"./ftp", perm='elradfmw')
+authorizer.add_anonymous(r"./ftp", perm='elradfmw')
 
 # 個々の接続を管理するハンドラーを作る
 handler = pyftpdlib.handlers.FTPHandler
 handler.authorizer = authorizer
-handler.abstract_fs = fs.FFS
+handler.abstracted_fs = fs.MyFileSystem
 
 # FTPサーバーを立ち上げる
-server = pyftpdlib.servers.FTPServer(("127.0.0.1", 10021), handler)
+server = pyftpdlib.servers.FTPServer(("0.0.0.0", 10021), handler)
 server.serve_forever()
